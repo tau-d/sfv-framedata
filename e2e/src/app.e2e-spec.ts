4c95f054595f8acb0df0f000f0dc6c8e3393c612
David Tau <alex.tau518@gmail.com>
2019-02-22 00:10:37 -0500

    fixed Travis and added more tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,10 +7,24 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
+  
+  it('should redirect to Abigail page', () => {
+    page.navigateTo().then(function() {
+      expect(browser.getCurrentUrl()).toBe(browser.baseUrl + '/Abigail');
+    });
+  });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to sfv-framedata!');
+  it('should navigate to selected character/mode pages', () => {
+    page.navigateTo().then(function() {
+      const newChar = 'Akuma';
+      page.selectCharacter(newChar).then(function() {
+        expect(browser.getCurrentUrl()).toBe(`${browser.baseUrl}/${newChar}`);
+      });
+      const newMode = 'vtOne';
+      page.selectCharacterMode(newMode).then(function() {
+        expect(browser.getCurrentUrl()).toBe(`${browser.baseUrl}/${newChar};charMode=${newMode}`);
+      })
+    });
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,24 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(path?: string) {
+    if (path) {
+      return browser.get(browser.baseUrl + '/' + path) as Promise<any>;
+    } else {
+      return browser.get(browser.baseUrl) as Promise<any>;
+    }    
   }
 
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+
+  selectCharacter(character: string) {
+    return element(by.id('charSelect')).element(by.cssContainingText('option', character)).click() as Promise<void>;
+  }
+
+  selectCharacterMode(charMode: string) {
+    return element(by.id('charModeSelect')).element(by.cssContainingText('option', charMode)).click() as Promise<void>;
+  }
+
 }
